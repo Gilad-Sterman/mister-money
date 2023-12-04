b2f6b6af069f889de4db85193462dc1ae8cae975
Gilad Sterman <giladsterman1999@gmail.com>
2023-12-04 17:21:33 +0200

    Updates

---
+++ assets/About-52eeda0f.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c as t,a as o}from"./index-9d703fee.js";const _={},c={class:"statistics"},n=o("h2",null,"This is page is still in development...",-1),a=[n];function i(l,r){return s(),t("div",c,a)}const u=e(_,[["render",i]]);export{u as default};
